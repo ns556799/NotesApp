d46f49e1ff2cc3982a434a5b1830bbb627a58b84
Niv Suresh <nivsuresh06@gmail.com>
2018-11-17 22:08:50 +0000

    Using fs to stringify and parse to read/write string

--- app.js
+++ app.js
@@ -6,9 +6,25 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
-const command = process.argv[2];
 const argv = yargs.argv;
+const command = argv._[0];
+
 const { title, body } = argv;
+
+const originalNote = {
+    title: 'This is my title',
+    body: 'This is my body'
+}
+
+const originalNoteString = JSON.stringify(originalNote)
+
+fs.writeFileSync('notes.json', originalNoteString)
+
+const noteString = fs.readFileSync('notes.json')
+const note = JSON.parse(noteString)
+
+console.log(typeof note)
+console.log(note.title)
 
 switch (command) {
     case 'add':
@@ -26,4 +42,4 @@ switch (command) {
      default:
        console.log('Command not recognised');
         break;    
-}
+} 

e3df8138d0d1a20fa185a4e38b752d88583016ee
Niv Suresh <nivsuresh06@gmail.com>
2018-11-17 20:07:24 +0000

    Using require to pull in user data

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('Starting app');
+
+const fs = require('fs');
+const os = require('os');
+const userInfo = os.userInfo();
+
+console.log(userInfo);
+
+fs.appendFileSync('greetings.txt', `Hello ${userInfo.username}`);

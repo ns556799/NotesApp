7db9a648f9fa920aaaae6c25903d4171a4f86f03
Niv Suresh <nivsuresh06@gmail.com>
2018-11-17 21:17:29 +0000

    Clean up + added lodash

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 console.log('Starting app');
 
 const fs = require('fs');
-const os = require('os');
-const userInfo = os.userInfo();
+const _ = require('lodash');
+
 const notes = require('./notes');
-
-let res = notes.addNote();
-
-let additionFunc = notes.addNumbers(5,10);
-
-console.log(additionFunc);
-// fs.appendFileSync('greetings.txt', `Hello ${userInfo.username}, you are ${notes.age}`);

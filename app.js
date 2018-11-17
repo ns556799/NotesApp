387bb74f872f6a101597a6722a0c67f25acf307a
Niv Suresh <nivsuresh06@gmail.com>
2018-11-17 20:23:13 +0000

    Creating an addition function

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const userInfo = os.userInfo();
 const notes = require('./notes');
 
 let res = notes.addNote();
-console.log(res);
 
+let additionFunc = notes.addNumbers(5,10);
+
+console.log(additionFunc);
 // fs.appendFileSync('greetings.txt', `Hello ${userInfo.username}, you are ${notes.age}`);
--- notes.js
+++ notes.js
@@ -4,3 +4,7 @@ module.exports.addNote = () => {
     console.log('addNote');
     return 'AddedNote';
 }
+
+module.exports.addNumbers = (a,b) => {
+    return `The addition of ${a} and ${b} = ${a+b}`;
+}

20c648f3b3cf26efcd09e1fc481b6cd251079c68
Niv Suresh <nivsuresh06@gmail.com>
2018-11-17 20:57:55 +0000

    Rollup

--- notes.js
+++ notes.js
@@ -3,8 +3,8 @@ console.log('Starting notes.js');
 module.exports.addNote = () => {
     console.log('addNote');
     return 'AddedNote';
-}
+};
 
 module.exports.addNumbers = (a,b) => {
     return `The addition of ${a} and ${b} = ${a+b}`;
-}
+};
